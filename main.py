5cb1f3e3bf5ea1953a9688e4227536065f2b2e6a
Bharath <youremail@gmail.com>
2026-08-04 17:45:39 +0530

    Added custom HTTP exception handling and status codes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -24,20 +25,39 @@ class NoteOut(BaseNote):
 fake_notes_db = []
 
 
+# Helper Function
+def note_not_found():
+    return HTTPException(
+        status_code=404,
+        detail="Note not found",
+        headers={"X-Error": "Invalid Note ID"}
+    )
+
+
+# Custom Exception
+class NegativeNumberException(Exception):
+    pass
+
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request, exc: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={
+            "message": "Negative numbers are not allowed."
+        }
+    )
+
+
 @app.get("/")
 def home():
     return {"message": "Notes API"}
 
 
 # Create Note
-@app.post(
-    "/notes/",
-    response_model=NoteOut,
-    status_code=status.HTTP_201_CREATED
-)
+@app.post("/notes/", response_model=NoteOut, status_code=status.HTTP_201_CREATED)
 def add_note(note: NoteCreate):
 
-    # Check duplicate ID
     for existing_note in fake_notes_db:
         if existing_note.id == note.id:
             raise HTTPException(
@@ -51,10 +71,13 @@ def add_note(note: NoteCreate):
 
 # Get Notes
 @app.get("/notes/", response_model=list[NoteOut])
-def get_notes(title: str = None):
+def get_notes(title: str = None, limit: int = 100):
+
+    if limit < 0:
+        raise NegativeNumberException()
 
     if title is None:
-        return fake_notes_db
+        return fake_notes_db[:limit]
 
     result = []
 
@@ -74,10 +97,7 @@ def update_note(note_id: int, updated_note: NoteCreate):
             fake_notes_db[index] = updated_note
             return updated_note
 
-    raise HTTPException(
-        status_code=404,
-        detail="Note not found"
-    )
+    raise note_not_found()
 
 
 # Delete Note
@@ -89,7 +109,4 @@ def delete_note(note_id: int):
             fake_notes_db.pop(index)
             return
 
-    raise HTTPException(
-        status_code=404,
-        detail="Note not found"
-    )
+    raise note_not_found()
